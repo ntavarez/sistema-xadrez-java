fca04f62e9156b3438a0d5439bcba39f0ee04f72
Natalia Tavares <natalia.h.tavares@gmail.com>
2020-03-31 20:51:50 -0300

    Primeira classe: Posição

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import board.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
