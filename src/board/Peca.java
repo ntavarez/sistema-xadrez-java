c2575b01dcbfe8e8c066ee7f3ef5252e123251a2
Natalia Tavares <natalia.h.tavares@gmail.com>
2020-03-31 21:00:48 -0300

    Implementando Tabuleiro e Peça

---
+++ src/board/Peca.java
@@ -0,0 +1,17 @@
+package board;
+
+public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+	
+
+}
